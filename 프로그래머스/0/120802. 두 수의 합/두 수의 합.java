c1496ee7abc62ac09f92b4d5453a5e7e1823afdf
chahyunsoo <98576976+chahyunsoo@users.noreply.github.com>
2024-02-22 17:45:55 +0900

    [level 0] Title: 두 수의 합, Time: 0.01 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/120802. 두 수의 합/두 수의 합.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = -1;
+        if(num1>=-50000 && num1<=50000 && num2>=-50000 && num2<=50000){
+            return num1+num2;
+        }
+        else
+            return answer;
+}
+}
