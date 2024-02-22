58087c2e6a1800fcc02e2481baa3c9b9d220ee14
chahyunsoo <98576976+chahyunsoo@users.noreply.github.com>
2024-02-22 17:42:44 +0900

    [level 2] Title: N-Queen, Time: 155.68 ms, Memory: 78.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12952. N－Queen/N－Queen.java
@@ -0,0 +1,30 @@
+class Solution {
+    static int answer=0;
+    static int[] arr;
+    
+    public int solution(int n) {
+        arr=new int[n];
+        queens(0,n);
+        return answer;
+    }
+    public static void queens(int depth,int n) {
+        if(depth==n){
+            answer++;
+            return;
+        }
+        else
+            for(int i=0; i<n; i++){
+                arr[depth]=i;
+                if(promising(depth)){
+                    queens(depth+1,n);
+                }
+            }
+    }
+    public static boolean promising(int depth){
+        for(int i=0; i<depth; i++){
+            if(arr[i]==arr[depth]){return false;}
+            if(Math.abs(arr[i]-arr[depth])==Math.abs(i-depth)){return false;}
+        }
+        return true;
+    }
+}
