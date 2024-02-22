155d8b56f744e33bc03f69fe977027cd1ea5cde7
chahyunsoo <98576976+chahyunsoo@users.noreply.github.com>
2024-02-22 17:46:02 +0900

    [level 0] Title: 숫자 비교하기, Time: 0.01 ms, Memory: 78.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(int num1, int num2) {
+        boolean s= (num1==num2);
+        if(!s)
+            return -1;
+        else
+            return 1;
+    }
+}
